9868925f128195543b669236ef22e259440dc95f
ajbecks <aaron_beckles@hotmail.co.uk>
2024-09-11 15:40:02 +0100

    Weather icon

--- src/components/weather/result/result.tsx
+++ src/components/weather/result/result.tsx
@@ -4,15 +4,12 @@ import ResultForecast from "./result_forecast/result_forecast"
 
 export default function Result(props: any) {
 	const { data } = props;
-	const currentTemp = Math.floor(data.main.temp)
-	const maxTemp = Math.floor(data.main.temp_max)
-	const minTemp = Math.floor(data.main.temp_min)
+
 
 	return (
         <>
             <h2>Result</h2>
-						<div>Today it is {currentTemp}° with a high of {maxTemp}° and a low of {minTemp}°</div>
-            <ResultForecast />
+            <ResultForecast data={data}/>
         </>
     )
 }
--- src/components/weather/result/result_forecast/result_forecast.tsx
+++ src/components/weather/result/result_forecast/result_forecast.tsx
@@ -1,7 +1,20 @@
-export default function ResultForecast() {
+
+
+export default function ResultForecast(data: any) {
+    console.log(data)
+    const currentTemp = Math.floor(data.data.main.temp)
+	const maxTemp = Math.floor(data.data.main.temp_max)
+	const minTemp = Math.floor(data.data.main.temp_min)
+    
     return (
         <>
             <h3>Result Forecast</h3>
+            <div>
+                <p>Current Temperature: {currentTemp}°C <img src={`https://openweathermap.org/img/wn/${data.data.weather[0].icon}@2x.png`} alt="" /></p>
+                <p>Max Temperature: {maxTemp}°C</p>
+                <p>Min Temperature: {minTemp}°C</p>
+
+            </div>
         </>
     )
 }
