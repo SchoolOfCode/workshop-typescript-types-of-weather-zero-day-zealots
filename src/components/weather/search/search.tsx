82f7b108425c64b03f5b01c5f62f55ac5006c3f9
Faisal Ahmed <git@fais.al>
2024-09-11 14:56:22 +0200

    basic rendering

--- src/components/weather/search/search.tsx
+++ src/components/weather/search/search.tsx
@@ -1,13 +1,36 @@
-import SearchBar from "./search_bar/search_bar"
-import SearchButton from "./search_button/search_button"
+import { useState } from "react";
+import Result from "../result/result";
 
 export default function Search() {
+	let [city, setCity] = useState("");
 
-    return (
-        <>
-            <h2>Search</h2>
-            <SearchBar />
-            <SearchButton />
-        </>
-    )
+	async function onSubmit(e: any) {
+		e.preventDefault();
+		let cityInput = e.target.value;
+		console.log(cityInput);
+		let resp = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f1f0b9e0659b64e4a75395bc9df2d8e7`
+		).then((res) => res.json());
+	}
+
+	
+	return (
+		<>
+			{city === "" ? (<form onSubmit={onSubmit}>
+				<label htmlFor="city">
+					<input
+						onChange={(e) => setCity(e.target.value)}
+						value={city}
+						type="text"
+						name="city"
+						id="city"
+						placeholder="City Name"
+						required
+					/>
+				</label>
+				<button type="submit">🔍</button>
+			</form>): <Result />}
+			
+		</>
+	);
 }
